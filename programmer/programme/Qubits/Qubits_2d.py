fcb5b7bc102c9b63ef169c2f7fa4bf0f175051b1
Chen <Gitzha0404@163.com>
2019-02-11 23:51:21 +0800

    update
    qubit_2d

--- programmer/programme/Qubits/Qubits_2d.py
+++ programmer/programme/Qubits/Qubits_2d.py
@@ -15,6 +15,7 @@ from functools import reduce
 class Qubits_2d():
     # 输入比特信息，驱动哈密顿量，参数
     # 得到单态的演化结果，整体保真度Ufidelity
+    # 内部的state和operator都是1*n,索引都是i*j
 
     # default evolution parameter
     default_options=Options()
@@ -35,7 +36,7 @@ class Qubits_2d():
         self.qubit_row , self.qubit_column = np.shape(self.frequency)
         if type(self.N_level) == int:
             self.N_level = self.N_level*np.ones_like(self.frequency)
-        if (not np.shape(self.frequency) == np.shape(self.eta_q) == np.shape(self.N_level)) or (not len(self.coupling)==2*len(self.frequency)-1):
+        if (not np.shape(self.frequency) == np.shape(self.eta_q) == np.shape(self.N_level)) or (not len(self.coupling)==2*len(self.frequency)-1) or (not np.shape(self.coupling)[1]+1==np.shape(self.frequency)[1]):
             print('dimension error')
             raise AssertionError()
         
@@ -49,7 +50,7 @@ class Qubits_2d():
         [self.E_eig,self.State_eig] = self.H0.eigenstates()
 
         #找到基态，以及各个比特的第一激发态的位置
-        self.first_excited = self._FirstExcite()
+        self.first_excited,self.ground_location = self._FirstExcite()
         # 确定演化的Rotation Wave Frame
         '''
         'NoRWF':No RWF
@@ -84,7 +85,6 @@ class Qubits_2d():
                         cmdstr.append(basis(self.N_level[JJ],2)*basis(self.N_level[JJ],2).dag())
                     else:
                         cmdstr.append(Qobj(np.zeros([self.N_level[JJ],self.N_level[JJ]])))
-
                 else:
                     cmdstr.append(qeye(self.N_level[JJ]))
             E_uc.append(tensor(*cmdstr))
@@ -147,11 +147,18 @@ class Qubits_2d():
         根据qubit参数，生成未加驱动的基本哈密顿量
         '''
         H0 = 0
-        for index in range(self.num_qubits):#添加qubit频率和非简谐性
-            H0 += self.frequency[index]*self.sm[index].dag()*self.sm[index] + self.eta_q[index]*self.E_uc[index]
+        for index_x in range(self.qubit_row):#添加qubit频率和非简谐性
+            for index_y in range(self.qubit_column):
+                H0 += self.frequency[index_x,index_y]*self.sm[index_x,index_y].dag()*self.sm[index_x,index_y] + self.eta_q[index_x,index_y]*self.E_uc[index_x,index_y]
         if self.num_qubits != 1:
-            for index in range(self.num_qubits-1):# 添加耦合
-                H0 += self.coupling[index]*(self.sm[index]+self.sm[index].dag())*(self.sm[index+1]+self.sm[index+1].dag())
+            for index_x in range(self.qubit_row):
+                for index_y in range(self.qubit_column):# 添加耦合
+                    if index_y != self.qubit_column-1:
+                        H0 += self.coupling[2*index_x,index_y]*(self.sm[index_x,index_y]+self.sm[index_x,index_y].dag())*(self.sm[index_x,index_y+1]+self.sm[index_x,index_y+1].dag())
+                    if index_x != self.qubit_row-1:
+                        H0 += self.coupling[2*index_x+1,index_y]*(self.sm[index_x,index_y]+self.sm[index_x,index_y].dag())*(self.sm[index_x+1,index_y]+self.sm[index_x+1,index_y].dag())
+            
+
         return(H0)
     def _strTostate(self,state):
         '''
@@ -201,9 +208,11 @@ class Qubits_2d():
     def _FirstExcite(self):
         '''
         找到基态，以及各个比特的第一激发态的位置
-        n个比特有n+1个值，前n个为n个比特第一激发态位置，最后一个为基态位置
+        first_excited是第一激发态位置,与比特位置一致
+        ground_location为基态位置
         '''
         first_excited = []
+        ground_location = []
         for II in range(0,self.num_qubits):#各个比特第一激发态位置
             state_label=''
             for JJ in range(0,self.num_qubits):
@@ -212,9 +221,11 @@ class Qubits_2d():
                 else:
                     state_label+='0'
             first_excited.append(self._findstate(state_label,search_space='brev'))
-        first_excited.append(self._findstate('0'*self.num_qubits,search_space='brev')) #基态的位置
+        first_excited = np.array(first_excited).reshape((self.qubit_row,self.qubit_column)) 
 
-        return(first_excited)
+        ground_location.append(self._findstate('0'*self.num_qubits,search_space='brev')) #基态的位置
+
+        return([first_excited,ground_location])
     
     
 
@@ -275,33 +286,34 @@ class Qubits_2d():
         生成时间t时刻的旋转坐标系矩阵
         '''
         U = []
-        for index in range(self.num_qubits):
-            if self.RWF=='CpRWF':
-                # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
-                mat = np.diag(np.ones(self.N_level[index],dtype = complex))
-                mat[1,1] = np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]])*select_time)
-                RW = Qobj(mat)
-                U.append(RW)
-            elif self.RWF=='UnCpRWF':
-                mat = np.diag(np.ones(self.N_level[index],dtype = complex))
-                mat[1,1] = np.exp(1j*(self.frequency[index])*select_time)
-                RW = Qobj(mat)
-                U.append(RW)
-                # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.frequency[index])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
-            elif self.RWF=='NoRWF':
-                U.append(qeye(self.N_level[index]))
-            elif self.RWF=='custom_RWF':
-                if type(self.RWA_freq) == float or type(self.RWA_freq) == int:
-                    self.RWA_freq = [self.RWA_freq]*self.num_qubits
-                # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]]+self.RWA_freq[index])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
-                mat = np.diag(np.ones(self.N_level[index],dtype = complex))
-                mat[1,1] = np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]]+self.RWA_freq[index])*select_time)
-                RW = Qobj(mat)
-                U.append(RW)
+        for index_x in range(self.qubit_row):
+            for index_y in range(self.qubit_column):
+                if self.RWF=='CpRWF':
+                    # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
+                    mat = np.diag(np.ones(self.N_level[index_x,index_y],dtype = complex))
+                    mat[1,1] = np.exp(1j*(self.E_eig[self.first_excited[index_x,index_y]]-self.E_eig[self.ground_location])*select_time)
+                    RW = Qobj(mat)
+                    U.append(RW)
+                elif self.RWF=='UnCpRWF':
+                    mat = np.diag(np.ones(self.N_level[index_x,index_y],dtype = complex))
+                    mat[1,1] = np.exp(1j*(self.frequency[index_x,index_y])*select_time)
+                    RW = Qobj(mat)
+                    U.append(RW)
+                    # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.frequency[index])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
+                elif self.RWF=='NoRWF':
+                    U.append(qeye(self.N_level[index_x,index_y]))
+                elif self.RWF=='custom_RWF':
+                    if type(self.RWA_freq) == float or type(self.RWA_freq) == int:
+                        self.RWA_freq = self.RWA_freq*np.ones_like(self.frequency)
+                    # U.append(basis(self.N_level[index],0)*basis(self.N_level[index],0).dag()+np.exp(1j*(self.E_eig[self.first_excited[index]]-self.E_eig[self.first_excited[-1]]+self.RWA_freq[index])*select_time)*basis(self.N_level[index],1)*basis(self.N_level[index],1).dag())
+                    mat = np.diag(np.ones(self.N_level[index_x,index_y],dtype = complex))
+                    mat[1,1] = np.exp(1j*(self.E_eig[self.first_excited[index_x,index_y]]-self.E_eig[self.ground_location]+self.RWA_freq[index_x,index_y])*select_time)
+                    RW = Qobj(mat)
+                    U.append(RW)
 
-            else:
-                error('RWF ERROR')
-        UF = tensor(*U)
+                else:
+                    error('RWF ERROR')
+            UF = tensor(*U)
         return(UF)
     def expect_evolution(self, operator):
         '''
@@ -318,10 +330,10 @@ class Qubits_2d():
         '''
         画出各个比特状态在Bloch球中的轨迹
         '''
-        nx = np.zeros([self.num_qubits,len(self.tlist)])
-        ny = np.zeros([self.num_qubits,len(self.tlist)])
-        nz = np.zeros([self.num_qubits,len(self.tlist)])
-        leakage = np.zeros([self.num_qubits,len(self.tlist)])
+        nx = np.zeros([self.qubit_row,self.qubit_column,len(self.tlist)])
+        ny = np.zeros([self.qubit_row,self.qubit_column,len(self.tlist)])
+        nz = np.zeros([self.qubit_row,self.qubit_column,len(self.tlist)])
+        leakage = np.zeros([self.qubit_row,self.qubit_column,len(self.tlist)])
         # 各个时间点，各个比特在X,Y,Z轴上投影
         
         # for t_index in range(len(self.tlist)):
@@ -335,43 +347,45 @@ class Qubits_2d():
         #         nz[q_index,t_index] = expect(opz,self.result.states[t_index])
         #         leakage[q_index,t_index] = expect(self.E_uc[q_index] , self.result.states[t_index])
 
-        for q_index in range(self.num_qubits):
+        for index_x in range(self.qubit_row):
+            for index_y in range(self.qubit_column):
             # opx = self.sm[q_index].dag()+self.sm[q_index]
             # opy = 1j*self.sm[q_index].dag()-1j*self.sm[q_index]
             # opz = (self.E_g[q_index]+self.E_e[q_index]+self.E_uc[q_index])-2*self.sm[q_index].dag()*self.sm[q_index]
-            opx = self.X_m[q_index]
-            opy = self.Y_m[q_index]
-            opz = self.E_g[q_index]-self.E_e[q_index]
-            nx[q_index] = self.expect_evolution(opx)
-            ny[q_index] = self.expect_evolution(opy)
-            nz[q_index] = self.expect_evolution(opz)
-            leakage[q_index] = self.expect_evolution(self.E_uc[q_index])
+                opx = self.X_m[index_x,index_y]
+                opy = self.Y_m[index_x,index_y]
+                opz = self.E_g[index_x,index_y]-self.E_e[index_x,index_y]
+                nx[index_x,index_y] = self.expect_evolution(opx)
+                ny[index_x,index_y] = self.expect_evolution(opy)
+                nz[index_x,index_y] = self.expect_evolution(opz)
+                leakage[index_x,index_y] = self.expect_evolution(self.E_uc[index_x,index_y])
         
         
         # 画图
-        fig,axes = plt.subplots(self.num_qubits,1)
-        for q_index in range(self.num_qubits):
-            if self.num_qubits > 1:
-                axes[q_index].plot(self.tlist,nx[q_index],label = 'x'+str(q_index))
-                axes[q_index].plot(self.tlist,ny[q_index],label = 'y'+str(q_index))
-                axes[q_index].plot(self.tlist,nz[q_index],label = 'z'+str(q_index))
-                axes[q_index].plot(self.tlist,leakage[q_index],label = 'leakage'+str(q_index))
-                axes[q_index].set_xlabel('t');axes[q_index].set_ylabel('population of qubit'+str(q_index));
-                axes[q_index].legend(loc = 'upper left')
-            else:
-                axes.plot(self.tlist,nx[q_index],label = 'x'+str(q_index))
-                axes.plot(self.tlist,ny[q_index],label = 'y'+str(q_index))
-                axes.plot(self.tlist,nz[q_index],label = 'z'+str(q_index))
-                axes[q_index].plot(self.tlist,leakage[q_index],label = 'leakage'+str(q_index))
-                axes.set_xlabel('t');axes.set_ylabel('population of qubit'+str(q_index));
-                axes.legend(loc = 'upper left')
+        fig,axes = plt.subplots(self.qubit_row,self.qubit_column)
+        for index_x in range(self.qubit_row):
+            for index_y in range(self.qubit_column):
+                if self.num_qubits > 1:
+                    axes[index_x,index_y].plot(self.tlist,nx[index_x,index_y],label = 'x'+str(index_x)+str(index_y))
+                    axes[index_x,index_y].plot(self.tlist,ny[index_x,index_y],label = 'y'+str(index_x)+str(index_y))
+                    axes[index_x,index_y].plot(self.tlist,nz[index_x,index_y],label = 'z'+str(index_x)+str(index_y))
+                    axes[index_x,index_y].plot(self.tlist,leakage[index_x,index_y],label = 'leakage'+str(index_x)+str(index_y))
+                    axes[index_x,index_y].set_xlabel('t');axes[index_x,index_y].set_ylabel('population of qubit'+str(index_x)+str(index_y));
+                    axes[index_x,index_y].legend(loc = 'upper left')
+                else:
+                    axes.plot(self.tlist,nx[index_x,index_y],label = 'x'+str(index_x)+str(index_y))
+                    axes.plot(self.tlist,ny[index_x,index_y],label = 'y'+str(index_x)+str(index_y))
+                    axes.plot(self.tlist,nz[index_x,index_y],label = 'z'+str(index_x)+str(index_y))
+                    axes[index_x,index_y].plot(self.tlist,leakage[index_x,index_y],label = 'leakage'+str(index_x)+str(index_y))
+                    axes.set_xlabel('t');axes.set_ylabel('population of qubit'+str(index_x)+str(index_y));
+                    axes.legend(loc = 'upper left')
 
 
-            sphere = Bloch()
-            sphere.add_points([nx[q_index] , ny[q_index] , nz[q_index]])
-            sphere.add_vectors([nx[q_index][-1],ny[q_index][-1],nz[q_index][-1]])
-            sphere.zlabel[0] = 'qubit'+str(q_index)+'\n$\\left|0\\right>$'
-            sphere.make_sphere()
+                sphere = Bloch()
+                sphere.add_points([nx[index_x,index_y] , ny[index_x,index_y] , nz[index_x,index_y]])
+                sphere.add_vectors([nx[index_x,index_y][-1],ny[index_x,index_y][-1],nz[index_x,index_y][-1]])
+                sphere.zlabel[0] = 'qubit'+str(index_x)+str(index_y)+'\n$\\left|0\\right>$'
+                sphere.make_sphere()
 
         plt.show()
 
@@ -428,7 +442,7 @@ class Qubits_2d():
                 state.insert(0,basis(self.N_level[JJ] , number))
                 if JJ == 0:
                     l += number
-                else:
+                else: #为了适应各个比特不同的能接结构
                     mullist = self.N_level[-1:-1-JJ:-1]
                     mulval = reduce(lambda x,y:x*y,mullist)
                     l += number*mulval
@@ -490,14 +504,14 @@ class Qubits_2d():
         '''
         对演化矩阵进行相位补偿，xita为每个比特的相位补偿角(弧度)
         '''
-        assert len(xita) == self.num_qubits
-
+        assert np.shape(xita) == np.shape(self.frequency)
+        xita_line = xita.reshape(1,self.num_qubits)
         for index in range(2**self.num_qubits):
             II = index
             for JJ in range(self.num_qubits):
                 number = np.int(np.mod(II,2))
                 if number == 1:
-                    process[:,index] = process[:,index]*np.exp(1j*xita[-1-JJ])
+                    process[:,index] = process[:,index]*np.exp(1j*xita_line[0][-1-JJ])
                 II = np.int(np.floor(II/2))
 
         return(process)
---
+++ programmer/programme/Qubits/test_2d.py
@@ -0,0 +1,69 @@
+import numpy as np
+from Qubits_2d import Qubits_2d
+from qutip import *
+import matplotlib.pyplot as plt
+
+def Z_pulse(t,args):
+
+    delta = args['delta']
+    T = args['T_P']
+    k = -0.61394;b = 2.87164*10**4;c = -delta*10**9;
+    x = (-b+np.sqrt(b**2-4*k*c))/2/k
+    if t<0:
+        w = 0
+    elif t>=0 and t<=3:
+        w = delta/2*(1-np.cos(np.pi/3*t))
+
+    # elif t>3 and t<=100:
+    #     w = delta
+    # elif t>100 and t<=150:
+    #     w = 1.01*delta
+    # elif t>150 and t<=T-3:
+    #     w = delta
+
+    elif t>3 and t<=T-3:
+        xt = x+0.005*x*np.cos(2*np.pi/50*t)
+        w = (k*xt**2+b*x)/10**9
+        
+        # w = delta
+
+    elif t>T-3 and t<=T:
+        w = delta/2*(1+np.cos(np.pi/3*(t-T+3)))
+    else:
+        w = 0
+    return(w)
+
+def readoutwave(t,args):
+    # t_start = args['t_start']
+    # t_end = args['t_end']
+    readoutfreq = args['readoutfreq']
+    # intensity = args['intensity']
+    w = 0.1*2*np.pi*np.cos(readoutfreq*(t))
+    return(w)
+
+if __name__ == '__main__':
+    Num_qubits = 2
+    frequency = np.array([
+        [5.1 , 5.1],
+        [5.1 , 5.1],
+    ])*2*np.pi
+    coupling = np.array([
+        [-0.250 , -0.250],
+        [-0.250 , -0.250],
+    ])*2*np.pi
+    eta_q=  np.array([
+        [-0.250 , -0.250],
+        [-0.250 , -0.250],
+    ])*2*np.pi
+    N_level= [3,2]
+    parameter = [frequency,coupling,eta_q,N_level]
+    QBE = Qubits_2d(qubits_parameter = parameter)
+    print(QBE)
+
+    args = {'T_P':100,'T_copies':101 , 'readoutfreq':frequency[0]}
+    H1 = [QBE.sm[0].dag()+QBE.sm[0],readoutwave]
+    Hdrive = [H1]
+
+    final = QBE.evolution(drive = None , psi = tensor((basis(3,1)).unit(),(basis(2,0)).unit()) ,  RWF = 'UnCpRWF' , RWA_freq = 0,track_plot = True ,argument = args)
+    print(final)
+    # final = QBE.process(drive = Hdrive,process_plot = True , RWF = 'custom_RWF' , RWA_freq = 0.0 , parallel = True , argument = args)
