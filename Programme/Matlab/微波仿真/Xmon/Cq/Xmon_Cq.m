82eb03159298b089bd5e49edc4f17aeaa5d2b9a2
Chen <Gitzha0404@163.com>
2020-08-20 02:02:00 +0800

    20200820

--- Programme/Matlab/微波仿真/Xmon/Cq/Xmon_Cq.m
+++ Programme/Matlab/微波仿真/Xmon/Cq/Xmon_Cq.m
@@ -8,5 +8,6 @@ f = Y.Frequencies;
 Y11 = squeeze(Y.Parameters(1,1,:));
 Cqlist = imag(Y11)/2/pi./f;
 % plot(f,Cqlist);
-Cq = imag(Y11(1))/2/pi./f(1);
+% Cq = imag(Y11(1))/2/pi./f(1);
+Cq = mean(Cqlist);
 disp(['Cq=',num2str(Cq)]);
