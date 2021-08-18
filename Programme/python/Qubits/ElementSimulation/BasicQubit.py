3e4a73fd0adb5dc9458a14159c2d3a4e73b89dc5
Chen <Gitzha0404@163.com>
2021-08-18 22:00:23 +0800

    update2010818

--- Programme/python/Qubits/ElementSimulation/BasicQubit.py
+++ Programme/python/Qubits/ElementSimulation/BasicQubit.py
@@ -1,3 +1,4 @@
+from operator import index
 import numpy as np
 from qutip import *
 from multiprocessing import Pool
@@ -9,105 +10,96 @@ class BasicQubit():
     '''
     def __init__(self , Hamilton , *args , **kwargs):
         self.__Hamilton = Hamilton #输入节点电容矩阵，电感矩阵，电阻矩阵，能级数目
-    # 生成基本的测量operator
+        # 生成基本的测量operator
         self.E_uc,self.E_e,self.E_g,self.X_m,self.Y_m= self._BasicMeasurementOperator()
+        #找到本征值和本征态
+        [self.energyEig,self.stateEig] = self.__Hamilton.eigenstates()
       
 
     def _BasicMeasurementOperator(self):
         '''
         生成构成用于测量的基本operator
         '''
+        Nlevel = self.__Hamilton.dims[0]
+        numQubit = len(Nlevel)
         E_uc = []
-        for II in range(0,self.__numQubit):
-            if self.__Nlevel[II]>2:
-                cmdstr=[basis(self.__numQubit[JJ],2)*basis(self.__numQubit[JJ],2).dag() if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(self.__numQubit)]                  
+        for II in range(0,numQubit):
+            if Nlevel[II]>2:
+                cmdstr=[basis(numQubit[JJ],2)*basis(numQubit[JJ],2).dag() if II==JJ else qeye(Nlevel[JJ]) for JJ in range(numQubit)]                  
             else:
-                cmdstr=[Qobj(np.zeros([self.__numQubit[JJ],self.__numQubit[JJ]])) if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(self.__numQubit)]
+                cmdstr=[Qobj(np.zeros([numQubit[JJ],numQubit[JJ]])) if II==JJ else qeye(Nlevel[JJ]) for JJ in range(numQubit)]
             E_uc.append(tensor(*cmdstr))
 
         E_e=[]
-        for II in range(0,self.__numQubit):
-            cmdstr=[basis(self.__Nlevel[JJ],1)*basis(self.__Nlevel[JJ],1).dag() if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(0,self.__numQubit)]
+        for II in range(0,numQubit):
+            cmdstr=[basis(Nlevel[JJ],1)*basis(Nlevel[JJ],1).dag() if II==JJ else qeye(Nlevel[JJ]) for JJ in range(0,numQubit)]
             E_e.append(tensor(*cmdstr))
         
         E_g=[]
-        for II in range(0,self.__numQubit):
-            cmdstr=[basis(self.__Nlevel[JJ],0)*basis(self.__Nlevel[JJ],0).dag() if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(0,self.__numQubit)]
+        for II in range(0,numQubit):
+            cmdstr=[basis(Nlevel[JJ],0)*basis(Nlevel[JJ],0).dag() if II==JJ else qeye(Nlevel[JJ]) for JJ in range(0,numQubit)]
             E_g.append(tensor(*cmdstr))
 
         X_m=[]
-        for II in range(0,self.__numQubit):
-            basisMatrix = np.zeros([self.__Nlevel[II],self.__Nlevel[II]])
+        for II in range(0,numQubit):
+            basisMatrix = np.zeros([Nlevel[II],Nlevel[II]])
             basisMatrix[0,1] = 1;basisMatrix[1,0] = 1
-            cmdstr=[Qobj(basisMatrix) if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(0,self.__numQubit)]
+            cmdstr=[Qobj(basisMatrix) if II==JJ else qeye(Nlevel[JJ]) for JJ in range(0,numQubit)]
             X_m.append(tensor(*cmdstr))    
 
         Y_m=[]
-        for II in range(0,self.__numQubit):
-            basisMatrix = np.zeros([self.__Nlevel[II],self.__Nlevel[II]])
+        for II in range(0,numQubit):
+            basisMatrix = np.zeros([Nlevel[II],Nlevel[II]])
             basisMatrix[0,1] = -1j;basisMatrix[1,0] = 1j
-            cmdstr=[Qobj(basisMatrix) if II==JJ else qeye(self.__Nlevel[JJ]) for JJ in range(0,self.__numQubit)] 
+            cmdstr=[Qobj(basisMatrix) if II==JJ else qeye(Nlevel[JJ]) for JJ in range(0,numQubit)] 
             Y_m.append(tensor(*cmdstr))  
 
-        return([E_uc,E_e, E_g , X_m, Y_m])
-    
+        return([E_uc,E_e, E_g , X_m, Y_m]) 
     def _strTostate(self,state):
         '''
         将0,1字符串转换为量子态
         '''
-        
-        qustate = [basis(self.__Nlevel[ii],int(eval(state[ii]))) for ii in range(len(state))]
+        Nlevel = self.__Hamilton.dims[0]
+        qustate = [basis(Nlevel[ii],int(eval(state[ii]))) for ii in range(len(state))]
         qustate = tensor(*qustate)
         return(qustate)
     def _numTostate(self,state):
         '''
         将0,1 int list 转换为量子态
         '''
-        qustate = [basis(self.__Nlevel[ii],int(state[ii])) for ii in range(len(state))]
+        Nlevel = self.__Hamilton.dims[0]
+        qustate = [basis(Nlevel[ii],int(state[ii])) for ii in range(len(state))]
         qustate = tensor(*qustate)
         return(qustate)
-    def _findstate(self,state,searchSpace='full',mark = 'string'):
+    def findstate(self,state,searchSpace='full',mark = 'string'):
         '''
-        在self.state中找到各个态对应的位置
+        在self.stateEig中找到state对应的index,对于简并的态，01探测到的是01+10，10探测到的是01-10
         '''
+        Nlevel = self.__Hamilton.dims[0]
+        numQubit = len(Nlevel)
         indexLevel = None
-        assert int(len(state)) == self.num_qubits
+        assert int(len(state.dims[0])) == numQubit
 
-        # e = np.zeros(self.num_qubits,dtype = 'int') #记录寻找的态对应每个比特上的能级
-        # s = np.zeros(self.num_qubits) #记录某个本征态的每个比特上相应能级的信息
-        # for i in range(self.num_qubits):
-        #     e[i] = int(eval(state[i]))
-
-        if search_space == 'full':
-            search_len=len(self.State_eig)
-        elif search_space == 'brev':
-            search_len=min(3*self.num_qubits,len(self.State_eig))
+        if searchSpace == 'full':
+            searchLen=len(self.State_eig)
+        elif searchSpace == 'brev':
+            searchLen=min(4*numQubit,len(self.stateEig))
         else:
             print('search space error')
-        if mark == 'string':
-            qustate = self._strTostate(state)
-            for index in range(search_len):
-                exp = expect(ket2dm(self.State_eig[index]),qustate)
-                if exp>0.5:
-                    index_level = index
-                    return(index_level)
-                # for i in range(self.num_qubits):
-                #     s[i] = np.abs(ptrace(self.State_eig[index],i)[e[i]][0][e[i]])
-                # if all(s>=0.5):
-                #     index_level = index
-                #     return(index_level)
-        elif mark == 'number':
-            qustate = self._numTostate(state)
-            for index in range(search_len):
-                exp = expect(ket2dm(self.State_eig[index]),qustate)
-                if exp>0.5:
-                    index_level = index
-                    return(index_level)
+
+        threshold = 0.08
+        probeResult = [expect(ket2dm(self.stateEig[ii]),state) for ii in range(searchLen)]
+        probeResultSorted = sorted(probeResult, reverse=True)
+        isDegenerated = abs(probeResultSorted[0]-probeResultSorted[1])<threshold
+    
+        if isDegenerated:
+            expect1 = (self.stateEig[probeResult.index(probeResultSorted[0])].dag()*state).data.toarray()[0][0]
+            expect2 = (self.stateEig[probeResult.index(probeResultSorted[1])].dag()*state).data.toarray()[0][0]
+            if expect1*expect2>0:
+                return(probeResult.index(probeResultSorted[0]))
+            else:
+                return(probeResult.index(probeResultSorted[1]))
         else:
-            print('Wrong Mark')
-
-
-
-        print('No State')
-        return(None)
+            return(probeResult.index(probeResultSorted[0]))
+            
 
